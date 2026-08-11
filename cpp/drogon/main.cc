a88d2a39db5671e660b980bfba884ab1de163751
Nakagome Tomoyuki <tnakagome@users.noreply.github.com>
2026-08-11 18:43:25 +0900

    Change listener port from 5555 to 55555

--- cpp/drogon/main.cc
+++ cpp/drogon/main.cc
@@ -1,7 +1,7 @@
 #include <drogon/drogon.h>
 
 int main() {
-    drogon::app().addListener("0.0.0.0", 5555);
+    drogon::app().addListener("0.0.0.0", 55555);
     drogon::app().run();
     return 0;
 }
